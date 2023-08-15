ebf548ef020d772e99ec85c1d7f01d35bc16c42e
조태중 <jtj3926@gmail.com>
2023-08-15 20:17:53 +0900

    deploy.yml, dataSorce로직 변경
    deploy.yml, dataSorce로직 변경

--- dataSource.ts
+++ dataSource.ts
@@ -7,11 +7,11 @@ dotenv.config();
 
 const dataSource = new DataSource({
   type: "mysql",
-  host: process.env.dbHost,
+  host: process.env.DBHOST,
   port: 3306,
-  username: process.env.dbUser,
-  password: process.env.dbPassword,
-  database: process.env.database,
+  username: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DATABASE,
   entities: [
    WeatherData,
    ObservationPointsInfo,
