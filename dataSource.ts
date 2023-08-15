04b5becf1f631deb35abf8ccb0e568969a7b4f50
조태중 <jtj3926@gmail.com>
2023-08-15 19:32:26 +0900

    데이터베이스 연결 수정
    데이터베이스 연결 수정

--- dataSource.ts
+++ dataSource.ts
@@ -2,18 +2,16 @@ import { DataSource } from "typeorm";
 import { ObservationPointsInfo } from "./src/entities/observation-points-info.entity";
 import { WeatherData } from "./src/entities/weather-datum.entity";
 
-import * as dotenv from 'dotenv';
-
-const environment = process.env.NODE_ENV || 'local';
-dotenv.config({ path: `.env.${environment}` });
+const dotenv = require("dotenv");
+dotenv.config();
 
 const dataSource = new DataSource({
   type: "mysql",
-  host: process.env.dbHost,
+  host: process.env.DBHOST,
   port: 3306,
-  username: process.env.dbUser,
-  password: process.env.dbPassword,
-  database: process.env.database,
+  username: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DATABASE,
   entities: [
    WeatherData,
    ObservationPointsInfo,
