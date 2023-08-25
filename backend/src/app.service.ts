cf9d759d1e195d60ffc301a78ab6acb931fafdd2
조태중 <jtj3926@gmail.com>
2023-08-25 20:21:32 +0900

    빌드테스트
    빌드테스트

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log(111);
+    console.log(111221);
     return 'Hello World!';
   }
 }
